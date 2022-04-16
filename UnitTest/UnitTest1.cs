5359bfba98c759ca626927cb3270b0245e607a57
宗彥 <s991110220@gmail.com>
2022-04-16 22:28:03 +0800

    新增第26項測試 根據使用者定義輸入12回傳FizzGozzRozz (通過)

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -436,6 +436,30 @@ namespace UnitTest
             const string expected = "Gozz";
             Assert.AreEqual(expected, actual);
         }
+        
+        [Test]
+        public void Use_User_Config2_And_Input_12_return_FizzGozzRozz()
+        {
+            //Arrange
+            var userConfig = new Dictionary<int, string>
+            {
+                { 3 , "Fizz"},
+                { 4 , "Gozz"},
+                { 5 , "Buzz"},
+                { 6 , "Rozz"},
+                { 7 , "Whizz"},
+            };
+            
+            var converter = new NumberConverter(userConfig);
+
+            //Act
+            const int replaceNumber = 12;
+            var actual = converter.Replace(replaceNumber);
+
+            //Assert
+            const string expected = "FizzGozzRozz";
+            Assert.AreEqual(expected, actual);
+        }
     }
 
     public class NumberConverter
