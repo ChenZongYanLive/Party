6a556bf28229aa17b3bc624e8a02b185b3b0c836
宗彥 <s991110220@gmail.com>
2022-04-16 20:52:31 +0800

    Create TDD Project

---
+++ NoUnitTest/NumberConverter.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace NoUnitTest
+{
+    public class NumberConverter
+    {
+        private readonly Dictionary<int, string> _numberDictionary = new Dictionary<int, string>
+        {
+            { 3 , "Fizz" },
+            { 5 , "Buzz" },
+            { 7 , "Whizz" },
+        };
+
+        public NumberConverter()
+        {
+            
+        }
+
+        public NumberConverter(Dictionary<int, string> userDictionary)
+        {
+            _numberDictionary = userDictionary;
+        }
+        
+        public string Replace(int number)
+        {
+            var replaceArray = new List<string>();
+
+            foreach (var (key, value) in _numberDictionary)
+            {
+                if(IsDivide(number, key) || HasNumber(number, key)) replaceArray.Add(value);
+            }
+
+            return replaceArray.Count > 0 ? string.Concat(replaceArray) : number.ToString();
+        }
+
+        /// <summary>
+        /// 確認數字中是否存在指定的數字
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="compareNumber"></param>
+        /// <returns></returns>
+        private static bool HasNumber(int number, int compareNumber)
+        {
+            return number.ToString().Contains(compareNumber.ToString());
+        }
+
+        /// <summary>
+        /// 數字是否可以被指定的數字整除
+        /// </summary>
+        /// <param name="number">被除數</param>
+        /// <param name="divisor">除數</param>
+        /// <returns></returns>
+        private static bool IsDivide(int number, int divisor)
+        {
+            return number % divisor == 0;
+        }
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using NoUnitTest;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var converter = new NumberConverter();
+            
+            Console.WriteLine("input 1, ans is : " + converter.Replace(1));
+            Console.WriteLine("input 2, ans is : " + converter.Replace(2));
+            Console.WriteLine("input 3, ans is : " + converter.Replace(3));
+            Console.WriteLine("input 6, ans is : " + converter.Replace(6));
+            Console.WriteLine("input 5, ans is : " + converter.Replace(5));
+            Console.WriteLine("input 10, ans is : " + converter.Replace(10));
+            Console.WriteLine("input 15, ans is : " + converter.Replace(15));
+            Console.WriteLine("input 30, ans is : " + converter.Replace(30));
+
+            Console.WriteLine();
+            Console.WriteLine("##########################################################");
+            Console.WriteLine();
+            
+            Console.WriteLine("input 13, ans is : " + converter.Replace(13));
+            Console.WriteLine("input 27, ans is : " + converter.Replace(27));
+            Console.WriteLine("input 52, ans is : " + converter.Replace(52));
+            Console.WriteLine("input 20, ans is : " + converter.Replace(20));
+            Console.WriteLine("input 35, ans is : " + converter.Replace(35));
+            Console.WriteLine("input 53, ans is : " + converter.Replace(53));
+            
+            Console.WriteLine();
+            Console.WriteLine("##########################################################");
+            Console.WriteLine();
+            
+            Console.WriteLine("input 7, ans is : " + converter.Replace(7));
+            Console.WriteLine("input 14, ans is : " + converter.Replace(14));
+            Console.WriteLine("input 17, ans is : " + converter.Replace(17));
+            Console.WriteLine("input 21, ans is : " + converter.Replace(21));
+            Console.WriteLine("input 35, ans is : " + converter.Replace(35));
+            
+            Console.WriteLine();
+            Console.WriteLine("##########################################################");
+            Console.WriteLine();
+
+            var userConfig = new Dictionary<int, string>
+            {
+                { 4 , "Gozz" },
+                { 6 , "Rozz" },
+            };
+            
+            converter = new NumberConverter(userConfig);
+            Console.WriteLine("input 4, ans is : " + converter.Replace(4));
+            Console.WriteLine("input 6, ans is : " + converter.Replace(6));
+            Console.WriteLine("input 8, ans is : " + converter.Replace(8));
+            Console.WriteLine("input 12, ans is : " + converter.Replace(12));
+            
+            Console.WriteLine();
+            Console.WriteLine("##########################################################");
+            Console.WriteLine();
+            
+            userConfig = new Dictionary<int, string>
+            {
+                { 3 , "Fizz" },
+                { 4 , "Gozz" },
+                { 5 , "Buzz" },
+                { 6 , "Rozz" },
+                { 7 , "Whizz" },
+            };
+            converter = new NumberConverter(userConfig);
+            Console.WriteLine("input 4, ans is : " + converter.Replace(4));
+            Console.WriteLine("input 6, ans is : " + converter.Replace(6));
+            Console.WriteLine("input 8, ans is : " + converter.Replace(8));
+            Console.WriteLine("input 12, ans is : " + converter.Replace(12));
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
