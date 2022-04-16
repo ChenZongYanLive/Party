da8e2ba5ed17e863132cc11091bc1c290b2abd81
宗彥 <s991110220@gmail.com>
2022-04-16 21:06:25 +0800

    新增第二項測試輸入2回傳2 (通過)

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -23,6 +23,21 @@ namespace UnitTest
             const string expected = "1";
             Assert.AreEqual(expected, actual);
         }
+        
+        [Test]
+        public void Input_2_return_2()
+        {
+            //Arrange
+            var converter = new NumberConverter();
+
+            //Act
+            const int replaceNumber = 2;
+            var actual = converter.Replace(replaceNumber);
+
+            //Assert
+            const string expected = "2";
+            Assert.AreEqual(expected, actual);
+        }
     }
 
     public class NumberConverter
