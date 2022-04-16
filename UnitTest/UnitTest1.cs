2a42a0c25fb42fbea9745d73e1b266c2f28bd2c2
宗彥 <s991110220@gmail.com>
2022-04-16 21:52:01 +0800

    Revert "重構 : 將"如果數字內有指定的數字" 抽成方法"
    This reverts commit c3a7b4cdb7119d3c6639d32d200972e86f07a1a9.

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -160,18 +160,13 @@ namespace UnitTest
 
             foreach (var divisorNumber in new int[]{ 3, 5 })
             {
-                if (IsDivide(replaceNumber, divisorNumber) || HasNumber(replaceNumber, divisorNumber))
+                if (IsDivide(replaceNumber, divisorNumber) || replaceNumber.ToString().Contains(divisorNumber.ToString()))
                 {
                     result +=  _replaceDictionary[divisorNumber];
                 }
             }
 
             return string.IsNullOrEmpty(result) ? replaceNumber.ToString() : result;
-        }
-
-        private static bool HasNumber(int replaceNumber, int divisorNumber)
-        {
-            return replaceNumber.ToString().Contains(divisorNumber.ToString());
         }
 
         private static bool IsDivide(int replaceNumber, int divisorNumber)
