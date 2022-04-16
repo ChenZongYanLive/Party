6ce98baee6e52559fd8c460ceef240b4fb0548d5
宗彥 <s991110220@gmail.com>
2022-04-16 21:58:18 +0800

    新增第12項測試輸入20回傳Buzz (通過)

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -174,6 +174,21 @@ namespace UnitTest
             const string expected = "Buzz";
             Assert.AreEqual(expected, actual);
         }
+        
+        [Test]
+        public void Input_20_return_Buzz()
+        {
+            //Arrange
+            var converter = new NumberConverter();
+
+            //Act
+            const int replaceNumber = 20;
+            var actual = converter.Replace(replaceNumber);
+
+            //Assert
+            const string expected = "Buzz";
+            Assert.AreEqual(expected, actual);
+        }
     }
 
     public class NumberConverter
